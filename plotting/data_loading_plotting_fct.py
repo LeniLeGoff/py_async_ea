3f057c7aefef01827f400b5e7408e5008ccb7f1c
Leni Le Goff <leni_legoff@proton.me>
2023-11-29 10:06:38 +0000

    update plotting script and notebook

--- plotting/data_loading_plotting_fct.py
+++ plotting/data_loading_plotting_fct.py
@@ -73,11 +73,13 @@ def load_learning_data_agg(folder):
     lt_file = open(folder + "/learning_trials")
     ld_file = open(folder + "/learning_delta")
     idx_file = open(folder + "/parent_indexes")
+    new_idx_file = open(folder + "/new_ind_indexes")
     time_file = open(folder + "/time_data")
 
     lt_lines = lt_file.readlines()
     ld_lines = ld_file.readlines()
     idx_lines = idx_file.readlines()
+    new_idx_lines = new_idx_file.readlines()
     time_lines = time_file.readlines()
     init_time = float(time_lines[0])
     time_data = [float(time_lines[i+1]) - float(time_lines[i]) for i in range(len(time_lines[:-1]))]
@@ -91,23 +93,25 @@ def load_learning_data_agg(folder):
     data_line = []
     iter = 0
     prev_idx = []
-    for lt_l, ld_l,idx_l, t, ct, dc in zip(lt_lines,ld_lines,idx_lines,time_data,cum_time_data, disc_time_data):
+    for lt_l, ld_l,idx_l, n_idx_l, t, ct, dc in zip(lt_lines,ld_lines,idx_lines,new_idx_lines,time_data,cum_time_data, disc_time_data):
         deltas = [float(ld) for ld in ld_l.split(",")]
         trials = [int(lt) for lt in lt_l.split(",")]
         data_line.append([t,ct,dc,200*cap_to(200,iter),iter, \
                         np.mean(deltas),np.median(deltas),np.var(deltas),np.max(deltas),np.min(deltas), \
                         np.mean(trials),np.median(trials),np.var(trials),np.max(trials),np.min(trials),])
-        iter+=len(list(set(idx_l.split(',')) - set(prev_idx)))
+        iter+=len(n_idx_l.split(','))
         prev_idx = idx_l.split(",")
     return data_line
 
 def load_morph_norms_agg(folder):
     mn_file = open(folder + "/morph_norms")
     idx_file = open(folder + "/parent_indexes")
+    new_idx_file = open(folder + "/new_ind_indexes")
     time_file = open(folder + "/time_data")
     
     mn_lines = mn_file.readlines()
     idx_lines = idx_file.readlines()
+    new_idx_lines = new_idx_file.readlines()
     time_lines = time_file.readlines()
     init_time = float(time_lines[0])
     time_data = [float(time_lines[i+1]) - float(time_lines[i]) for i in range(len(time_lines[:-1]))]
@@ -122,7 +126,7 @@ def load_morph_norms_agg(folder):
     prev_iter = 0
     prev_idx = []
     new_ind = 0
-    for mn_l,idx_l, t,ct,dt in zip(mn_lines,idx_lines, time_data,cum_time_data,disc_time_data):
+    for mn_l,idx_l,n_idx_l, t,ct,dt in zip(mn_lines,idx_lines, new_idx_lines, time_data,cum_time_data,disc_time_data):
         norms = [float(mn) for mn in mn_l.split(",")]
         if(prev_iter == cap_to(200,iter)):
             new_ind += len(list(set(idx_l.split(',')) - set(prev_idx)))
@@ -130,7 +134,7 @@ def load_morph_norms_agg(folder):
             new_ind = len(list(set(idx_l.split(',')) - set(prev_idx)))
         data_line.append([t,ct,dt,200*cap_to(200,iter),new_ind,np.mean(norms),np.median(norms),np.var(norms),np.max(norms),np.min(norms)])
         prev_iter = cap_to(200,iter)
-        iter+=len(list(set(idx_l.split(',')) - set(prev_idx)))
+        iter+=len(n_idx_l.split(','))
         prev_idx = idx_l.split(",")
     return data_line
 
